ad3bc0dff6e9ef462ab8167eb91f7144b7c91b7c
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2024-05-21 13:10:10 -0400

    final

--- main/runner.py
+++ main/runner.py
@@ -23,7 +23,7 @@ def runner(config_name, seed, generate_figures=True):
     wandb_run = wandb.init(
         name=config_name,
         project='gcpc_velocity',
-        entity='iyer',
+        entity='', # redacted for anonymity
         config=config, reinit=True, mode='online'
     )
 
@@ -236,5 +236,5 @@ def save_figures(wandb_run, config, model, train_dataset, test_dataset, pred_spa
 
 
 if __name__ == '__main__':
-    runner('bird3d', seed=103)
-    runner('bird3d', seed=104)
+    # Usage: runner({config name}, seed={some number})
+    # Example: runner('blob', seed=100)
